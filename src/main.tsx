4d0ea4df9bc29c925ef971b443d48e15712efdcd
Diana Muzhylivska <diana.listen@gmail.com>
2026-07-02 15:41:35 +0300

    add props in components App.tsx and Product.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,19 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+// src/main.tsx
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+// import React from "react";
+// import ReactDOM from "react-dom/client";
+// import App from "./App";
+
+// ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
+
+// src/main.tsx
+
+import { createRoot } from "react-dom/client";
+import App from "./components/App";
+
+createRoot(document.getElementById("root") as HTMLElement).render(<App />);
+
