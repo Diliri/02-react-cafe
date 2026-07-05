c14a277a0e9f1739224f60c62fa9a0c8a7a2f2dc
Diana Muzhylivska <diana.listen@gmail.com>
2026-07-05 13:06:03 +0300

    add VoteOptions component + App update

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,50 @@
 // src/App.tsx
+import { useState } from "react";
 import css from "./App.module.css";
 import CafeInfo from "./components/CafeInfo/CafeInfo.tsx";
+import VoteOptions from "./components/VoteOptions/VoteOptions.tsx";
+import VoteStats from "./components/VoteStats/VoteStats.tsx";
+//import CafeInfo from "./components/CafeInfo/CafeInfo.tsx";
+
+import type { Votes, VoteType } from "./types/votes.ts";
 
 export default function App() {
+
+  // Стан для об'єкта голосів
+  const [votes, setVotes] = useState<Votes>({ good: 0, neutral: 0, bad: 0 });
+
+  // Правильна функція для голосування (оновлює конкретне поле)
+  const handleVote = (type: VoteType) => {
+    setVotes((prevVotes) => ({
+      // spread оператор копіює усі значення в новий масив
+      ...prevVotes,
+      [type]: prevVotes[type] + 1,
+    }));
+  };
+
+  // Функція для скидання стану в початковий вигляд
+  const resetVotes = () => {
+    setVotes({ good: 0, neutral: 0, bad: 0 });
+  };
+
   return (
     <>
       <div className={css.app}>
         <CafeInfo />
+        {/* Передача функції та стану через пропси */}
+        <VoteOptions 
+          onVote={handleVote} 
+          onReset={resetVotes} 
+          canReset={true} 
+        />
+    
+        <VoteStats 
+          votes={votes} 
+          totalVotes={0} 
+          positiveRate={0} 
+        />
       </div>
+
     </>
   );
 }
